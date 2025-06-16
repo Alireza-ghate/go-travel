f6c4b1796125d2fcf539cf341c6132da3a02aeb7
Alireza-ghate <alireza.ghate.jd@gmail.com>
2025-06-16 13:47:57 +0330

    setup project vite - tailwind - eslint

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Page from "./pages/Page";
+
+function App() {
+  return (
+    <div className="font-bold text-blue-600">
+      Hello, world!
+      <Page />
+    </div>
+  );
+}
+
+export default App;
