ec01f41e9dd23f30c30851734c3921095bc2ce1a
Alireza-ghate <alireza.ghate.jd@gmail.com>
2025-07-29 19:13:53 +0330

    install and setup react query + supabase client

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ExploreMore from "./components/ExploreMore/ExploreMore";
 import Footer from "./components/Footer";
 import FrequentTraveler from "./components/FrequentTraveler";
@@ -10,25 +11,33 @@ import Page from "./components/Page";
 import Services from "./components/Services/Services";
 import Steps from "./components/Steps/Steps";
 import Testimonials from "./components/Testimonials/Testimonials";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+// Create a client
+const queryClient = new QueryClient();
 
 function App() {
   // Page is like a container or wrapper
   return (
-    <Page>
-      <Header>
-        <Navigation />
-        <Hero />
-      </Header>
-      <Main>
-        <Steps />
-        <Services />
-        <News />
-        <ExploreMore />
-        <FrequentTraveler />
-        <Testimonials />
-      </Main>
-      <Footer />
-    </Page>
+    // Provide the client to App
+    <QueryClientProvider client={queryClient}>
+      <Page>
+        <Header>
+          <Navigation />
+          <Hero />
+        </Header>
+        <Main>
+          <Steps />
+          <Services />
+          <News />
+          <ExploreMore />
+          <FrequentTraveler />
+          <Testimonials />
+        </Main>
+        <Footer />
+      </Page>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
---
+++ src/api/api.ts
@@ -0,0 +1,48 @@
+import { createClient } from "@supabase/supabase-js";
+import { BlogPost } from "../utils/contentTypes";
+import { Database } from "./Database";
+
+const supabaseUrl = "https://fnkzsitgfwelmhqqgqqb.supabase.co";
+const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
+// create supabase client
+const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+
+//create functions for fetching data from our tables in supabase using this supabase client
+export async function getBlogPost() {
+  const { data, error } = await supabase.from("BlogPosts").select("*");
+
+  // handled by react query
+  if (error)
+    throw new Error(
+      `ERROR: database returned error when fetching blog posts ${error.message}`,
+    );
+
+  //   BlogPost = {
+  //   id: string;
+  //   img: string;
+  //   alt: string;
+  //   date: string;
+  //   title: string;
+  //   summary: string;
+  // }
+  // post {
+  //   id,
+  //   img_url,
+  //   article_title,
+  //   ...
+  // }
+
+  // change properties with _ to normal properties
+  const blogPosts: BlogPost[] = data.map((post) => {
+    return {
+      id: post.id,
+      alt: post.img_alt,
+      date: post.date_created,
+      img: post.img_url,
+      summary: post.article_summary,
+      title: post.article_title,
+    };
+  });
+
+  return blogPosts;
+}
